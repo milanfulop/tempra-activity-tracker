cf36cba060260736d720251ad576684c575594dd
Milan <darklionmail0@gmail.com>
2026-03-26 11:54:14 +0100

    feat: flutter dotenv

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'features/home/presentation/screens/home.dart';
 import 'features/statistics/presentation/screens/statistics.dart';
-void main() {
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: 'assets/.env', isOptional: true);
   runApp(const MyApp());
 }
 
